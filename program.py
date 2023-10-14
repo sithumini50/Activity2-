d52c90130ed733de70c91984f531ecf66b5e8369
Sithumini50 <sithuminijayasekara50@gmail.com>
2023-10-14 16:07:46 +0530

    added square and cube

--- program.py
+++ program.py
@@ -7,9 +7,13 @@ subtraction = a - b
 multiplication = a * b
 division = a / b
 remainder = a % b
+square = a ** 2
+cube = a ** 3
 
 print(f"Addition: {addition}")
 print(f"Subtraction: {subtraction}")
 print(f"Multiplication: {multiplication}")
 print(f"Division: {division}")
 print(f"Remainder: {remainder}")
+print(f"Square: {square}")
+print(f"Cube: {cube}")
