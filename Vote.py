76b2614cdc802805ed74a7b042b7e04c0d7c0da4
Sithumini50 <sithuminijayasekara50@gmail.com>
2023-10-14 16:05:40 +0530

    added information about voter registration

--- Vote.py
+++ Vote.py
@@ -1,10 +1,15 @@
-
 age = int(input("Enter your age: "))
 
 if age >= 18:
     print("You are eligible to vote. Exercise your right to vote!")
+    registered = input("Have you registered to vote? (yes/no): ").lower()
+    if registered == "yes":
+        print("Great! Make sure to cast your vote in the upcoming election.")
+    else:
+        print("Please consider registering to vote. It's an important civic duty."
 else:
-    print("Sorry, you are not eligible to vote yet. Please wait until you are 18 or older.")
+        print("Sorry, you are not eligible to vote yet.")
+
     years_to_wait = 18 - age
     if years_to_wait > 1:
         print(f"You'll be eligible to vote in {years_to_wait} years.")
