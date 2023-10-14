1faa3cad6b49182aa63f9efd8f540b4f621eceff
Sithumini50 <sithuminijayasekara50@gmail.com>
2023-10-14 14:55:59 +0530

    initial commit

---
+++ program.py
@@ -0,0 +1,12 @@
+
+a = 10
+b = 5
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
