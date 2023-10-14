c9f96f1404437e800007f8decdf1c85455863444
Sithumini50 <sithuminijayasekara50@gmail.com>
2023-10-14 14:59:04 +0530

    added remainder

--- program.py
+++ program.py
@@ -6,7 +6,10 @@ addition = a + b
 subtraction = a - b
 multiplication = a * b
 division = a / b
+remainder = a % b
+
 print(f"Addition: {addition}")
 print(f"Subtraction: {subtraction}")
 print(f"Multiplication: {multiplication}")
 print(f"Division: {division}")
+print(f"Remainder: {remainder}")
